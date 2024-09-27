3c00f1a0432d6f6ea43768b1027bee15f738d9af
Aviral Shastri <104085688+aviralshastri@users.noreply.github.com>
2024-09-27 22:38:01 +0530

    fixing routing problem
    fixing the variable defining and routing problem for setup webpage.

--- device-code/main/SETUP.h
+++ device-code/main/SETUP.h
@@ -1,40 +1,12 @@
 #ifndef SETUP_H
 #define SETUP_H
 
-#include <WiFi.h>
-#include <WebServer.h>
+#include <Arduino.h>
 
 class SETUP {
-private:
-    WebServer server;
-    
-    const char* apSSID;
-    const char* apPassword;
-    const char* validUsername;
-    const char* validPassword;
-
-    bool isLoggedIn;  // To track if user is logged in
-
-    // HTML pages
-    const char* loginPage;
-    const char* homePage;
-
-    // Private methods to serve pages
-    void handleLogin();
-    void handleHome();
-
-    // Private method to check login status
-    void checkLogin();
-
 public:
-    // Constructor
-    SETUP(const char* ssid, const char* password, const char* username, const char* passwordInput);
-
-    // Public method to start the access point and web server
-    void begin();
-
-    // Public method to process server requests
-    void handleClient();
+    static const char* login;
+    static const char* home;
 };
 
-#endif
+#endif 
--- device-code/main/main.ino
+++ device-code/main/main.ino
@@ -1,18 +1,82 @@
 #include <WiFi.h>
-#include "SETUP.h"
+#include <WebServer.h>
+#include <SETUP.h>
 
-const char* apSSID = "ESP32_Access_Point";
-const char* apPassword = "123456789";
-const char* validUsername = "admin";
-const char* validPassword = "admin";
+const char* ssid = "ESP32_Access_Point";
+const char* password = "12345678";
 
-SETUP setupManager(apSSID, apPassword, validUsername, validPassword);
+WebServer server(80);
+
+bool isAuthenticated = false;
+
+const char* loginPage = SETUP::login;
+
+const char* homePage = SETUP::home;
 
 void setup() {
-    Serial.begin(115200);
-    setupManager.begin();
+  Serial.begin(115200);
+  
+  WiFi.softAP(ssid, password);
+  Serial.println("Access Point Started");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.softAPIP());
+
+  server.on("/", handleRoot);
+  server.on("/login", HTTP_POST, handleLogin);
+  server.on("/home", handleHome);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-    setupManager.handleClient();
+  server.handleClient();
+}
+
+void handleRoot() {
+  Serial.println("Handling root request");
+  if (isAuthenticated) {
+    Serial.println("User is authenticated, redirecting to home");
+    server.sendHeader("Location", "/home");
+    server.send(303);
+  } else {
+    Serial.println("User is not authenticated, sending login page");
+    server.send(200, "text/html", loginPage);
+  }
+}
+
+void handleLogin() {
+  Serial.println("Handling login request");
+  if (server.hasArg("username") && server.hasArg("password")) {
+    if (server.arg("username") == "admin" && server.arg("password") == "admin") {
+      isAuthenticated = true;
+      Serial.println("Login successful, redirecting to home");
+      server.sendHeader("Location", "/home");
+      server.send(303);
+    } else {
+      Serial.println("Invalid credentials");
+      server.send(401, "text/plain", "Invalid credentials");
+    }
+  } else {
+    Serial.println("Bad login request");
+    server.send(400, "text/plain", "Bad Request");
+  }
+}
+
+void handleHome() {
+  Serial.println("Handling home request");
+  if (!isAuthenticated) {
+    Serial.println("User is not authenticated, redirecting to root");
+    server.sendHeader("Location", "/");
+    server.send(303);
+  } else {
+    Serial.println("User is authenticated, sending home page");
+    server.send(200, "text/html", homePage);
+  }
+}
+
+void handleNotFound() {
+  Serial.println("Handling 404 request");
+  server.send(404, "text/plain", "404: Not found");
 }
